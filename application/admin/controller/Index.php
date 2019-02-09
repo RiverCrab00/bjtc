717ccc2cd166477ad6ada6652c2741fc8d3086f5
RiverCrab <sdjnzyliang@sina.com>
2019-02-09 22:21:28 +0800

    111

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace app\admin\controller;
-
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
     function index()
     {
+        halt(Db::table('users')->all());
         $this->fetch();
     }
     function login()
